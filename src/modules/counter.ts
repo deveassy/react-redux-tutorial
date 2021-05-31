25ce88aadbeaef0e98d1f923d603bf30e75f60bc
deveassy <xjfmwld@gmail.com>
2021-05-31 17:26:27 +0900

    프로젝트생성, 카운터 리덕스모듈 작성

---
+++ src/modules/counter.ts
@@ -0,0 +1,41 @@
+// action type declaration
+const INCREASE = "counter/INCREASE" as const;
+const DECREASE = "counter/DECREASE" as const;
+const INCREASE_BY = "couner/INCREASE_BY" as const;
+
+// action create function declaration
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const increaseBy = (diff: number) => ({
+  type: INCREASE_BY,
+  payload: diff,
+});
+
+// function return type declaration
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+function counter(state: CounterState, action: CounterAction) {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
